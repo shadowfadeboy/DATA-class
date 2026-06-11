ce55573d1a859ab959453c7775e954b2970876d6
shadowfadeboy <shadowkingg205@gmail.com9o9o>
2026-06-11 14:33:08 +0530

    task2

---
+++ day2task.py
@@ -0,0 +1,27 @@
+#basic
+name=input("enter your name")
+city=input("city?")
+print("Hello ",name)
+print("welcome to",city)
+
+#calculator
+a=int(input("enter one number"))
+b=int(input("enter another number"))
+print("add -",a+b)
+print("sub -",a-b)
+print("multiply -",a*b)
+print("divide -",a/b)
+
+#temperature convertor
+f=int(input("enter degree -"))
+c=(f-32)*5/9
+print("enter Fahrenheit :",f)
+print("celsius :",c)
+
+#task4
+name=input("enter name")
+a=int(input("enter one number -"))
+b=int(input("enter another number -"))
+print("hello ",name)
+print("sum is:",a+b)
+print("lenght of name :",len(name))
