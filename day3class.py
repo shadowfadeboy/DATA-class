a7e0725d69c33277bfab6310a75a068d0126d670
shadowfadeboy <shadowkingg205@gmail.com9o9o>
2026-06-12 14:36:50 +0530

    task2 completed

---
+++ day3class.py
@@ -0,0 +1,41 @@
+#arithmetic operators
+#exponentatiion(power ),floor division(//,quotient)
+"""a=9
+b=2
+print(a**b)
+print(a//b)"""
+
+#comparision operaotr
+# ==
+# >
+# <
+# !=
+# >=
+# <=
+
+
+#logical operators
+# and
+# or
+# not
+# a=3
+# b=4
+# 
+# print("logicla not",not(a==b))
+# print("logical not",not(True))
+ #assignment 
+# +=
+# -=
+# *=
+# /=
+# %=
+# //=
+# **=
+
+a=5
+b=10
+a+=5   # a=a+5
+b**=10 # b=b**10
+print(a)
+print(b)
+
---
+++ daytask2.py
@@ -0,0 +1,41 @@
+#arithmetic operator
+"""a=2
+b=4
+print("add",a+b)
+print("sub",a-b)
+print("multiply",a*b)
+print("division",a/b)
+print("modulous",a%b)
+print("floor division",a//b)
+print("exponentation",a**b)"""
+
+
+# #comparision operator
+"""a=7
+b=10
+print("equal to ",a==b)
+print("not equal to ",a!=b)
+print("greater than ",a>b)
+print("samller than ",a<b)
+print("greater than or equal to ",a>=b)
+print("smaller than or equal to ",a<=b)"""
+
+#logical operator
+"""a=9
+b=10
+print("logical and",(a<b) and(b>a))
+print("logical or",(a==b) or (b!=a))
+print("logical and",not(a==b))"""
+
+#assignment operator
+"""a=5
+a+=5
+a-=2
+a*=4
+a//=2
+a/=2
+a%=3
+a**=3
+print("result of a:",a)"""
+
+
